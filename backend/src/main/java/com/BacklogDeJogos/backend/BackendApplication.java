6570d3447bd551b6730c8b5e9c8dd6d2a69ff45a
MuriloLenharo01 <Murilolenharo01@gmail.com>
2026-09-22 15:41:36 -0300

    Model

--- backend/src/main/java/com/BacklogDeJogos/backend/BackendApplication.java
+++ backend/src/main/java/com/BacklogDeJogos/backend/BackendApplication.java
@@ -1,13 +1,84 @@
 package com.BacklogDeJogos.backend;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BackendApplication {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+@Entity
+@Table(name = "Game")
+
+public class Game {
+	@id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String name; 
+	
+	@Column(nullable = false, length = 255)
+	private String description;
+	
+	@Column(nullable = false)
+	private double value;
+	
+	@Column(nullable = false)
+	private int quantity;
+
+	public Game() {
+
 	}
 
+	public Games(String name, String description, double value, int quantity) {
+		this.name = name;
+		this.description = description;
+		this.value = value;
+		this.quantity = quantity;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@override
+	public String toString() {
+		return "Game [id=" + id + ", name=" + name + ", description=" + description + ", value=" + value
+				+ ", quantity=" + quantity + "]";
+	}
 }
+
